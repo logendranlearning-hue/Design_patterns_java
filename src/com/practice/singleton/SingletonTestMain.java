6d702752ab239a2b62fe307d809850d683b02155
logendran.learning <logendra.learning@gmail.com>
2026-01-31 21:08:17 +0530

    Singleton design pattern added comments

--- src/com/practice/singleton/SingletonTestMain.java
+++ src/com/practice/singleton/SingletonTestMain.java
@@ -1,5 +1,30 @@
 package com.practice.singleton;
 
+/**
+ * Main class to test different singleton implementations.
+ *
+ * Real-World Applications of the Singleton Pattern
+ * Logging Systems : Maintain a consistent logging mechanism across an application.
+ * Configuration Managers : Centralize access to configuration settings.
+ * Database Connections : Manage a single point of database access.
+ * Thread Pools : Efficiently manage a pool of threads for concurrent tasks.
+ * Cache Managers, Print Spoolers (Single Printer Queue) and Runtime Environments ( java.lang.Runtime is a singleton)
+ *
+ * Features of the Singleton Design Pattern
+ * Single Instance: Ensures only one object of the class exists in the JVM.
+ * Global Access Point: Provides a centralized way to access the instance.
+ * Lazy or Eager Initialization: An Instance can be created at class load time (eager) or when first needed (lazy).
+ * Thread Safety: Can be designed to work correctly in multithreaded environments.
+ * Resource Management: Useful for managing shared resources like configurations, logging or database connections.
+ * Flexibility in Implementation: Can be implemented using eager initialization, lazy initialization, double-checked locking or an inner static class.
+ * When would you like to use the Singleton Method Design Pattern
+ * Use the Singleton method Design Pattern when:
+ *
+ * Consider using the Singleton pattern when you need to ensure that only one instance of a class exists in your application.
+ * If you think you might want to extend the class later, the Singleton pattern is a good choice. It allows for subclassing, so clients can work with the extended version without changing the original Singleton.
+ * It's important to use it judiciously. Overuse can lead to issues such as hidden dependencies and testing difficulties.
+ *
+ */
 public class SingletonTestMain {
 
     public static void main(String[] args) {
